c07a849edcc7c2c03be380f4be1be2d471bc67cd
ericj <ericjyyang@gmail.com>
2023-03-26 02:14:09 -0400

    created sql database

---
+++ sql_files/queries.sql
@@ -0,0 +1,131 @@
+create table premier_league (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+
+create table la_liga (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table serie_a (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table bundesliga (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table ligue_1 (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table aus_bundesliga (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table jupiler (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table eredivisie (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table liga_portugal (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table premier_liga (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
+	
+create table super_lig (
+	Club varchar primary key not null,
+	Country varchar,
+	League varchar,
+	Market_Value decimal,
+	Pl int,
+	W int,
+	D int,
+	L int,
+	Pts int,
+	Pts_Per_Match decimal)
