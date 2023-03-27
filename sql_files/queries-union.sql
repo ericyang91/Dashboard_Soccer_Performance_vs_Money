1bfb8775ee55d3b428a7daa3ff3879ce8a672d43
ericj <ericjyyang@gmail.com>
2023-03-27 17:53:29 -0400

    added union query and started flask

---
+++ sql_files/queries-union.sql
@@ -0,0 +1,35 @@
+create table all_leagues as
+select *
+from aus_bundesliga
+union all
+select *
+from bundesliga
+union all
+select *
+from eredivisie
+union all
+select *
+from jupiler
+union all
+select *
+from la_liga
+union all
+select *
+from liga_portugal
+union all
+select *
+from ligue_1
+union all
+select *
+from premier_league
+union all
+select *
+from premier_liga
+union all
+select *
+from serie_a
+union all
+select *
+from super_lig;
+
+select * from all_leagues
